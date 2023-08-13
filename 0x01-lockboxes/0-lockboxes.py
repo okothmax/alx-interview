4895fde162968e3af93c6e674222b75d359fe766
okothmax <okothmaxwell03@gmail.com>
2023-08-13 14:21:56 +0300

    added readme

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -17,16 +17,21 @@ def canUnlockAll(boxes):
             one to navigate through the list, and the other to verify 
             whether a given key matches an index for the purpose of unlocking.
     """
-    if type(boxes) is not list:
+    n = len(boxes)
+    if n == 0:
         return False
-    elif (len(boxes)) == 0:
-        return False
-    for k in range(1, len(boxes) - 1):
-        boxes_checked = False
-        for idx in range(len(boxes)):
-            boxes_checked = k in boxes[idx] and k != idx
-            if boxes_checked:
-                break
-        if boxes_checked is False:
-            return boxes_checked
-    return True
+
+    unlocked_boxes = [False] * n
+    unlocked_boxes[0] = True
+
+    keys_stack = [0]
+
+    while keys_stack:
+        current_box = keys_stack.pop()
+
+        for key in boxes[current_box]:
+            if key < n and not unlocked_boxes[key]:
+                unlocked_boxes[key] = True
+                keys_stack.append(key)
+
+    return all(unlocked_boxes)
